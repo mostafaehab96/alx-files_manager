b993f5549375b892b9d39e13a2d18391ef381bc1
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2024-02-16 03:41:30 +0200

    Hanlde some error

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -7,16 +7,20 @@ const getConnect = async (req, res) => {
   const authHeader = req.headers.authorization;
 
   if (authHeader) {
-    const credentials = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    const email = credentials[0];
-    let password = credentials[1];
-    password = sha1(password);
-    const user = await dbClient.users.findOne({ email, password });
-    if (user) {
-      const token = uuid.v4();
-      const key = `auth_${token}`;
-      await redisClient.set(key, user._id.toString(), 60 * 60 * 24);
-      return res.json({ token });
+    try {
+      const credentials = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+      const email = credentials[0];
+      let password = credentials[1];
+      password = sha1(password);
+      const user = await dbClient.users.findOne({ email, password });
+      if (user) {
+        const token = uuid.v4();
+        const key = `auth_${token}`;
+        await redisClient.set(key, user._id.toString(), 60 * 60 * 24);
+        return res.json({ token });
+      }
+    } catch (err) {
+      return res.status(401).json({ error: 'Unauthorized' });
     }
   }
 
